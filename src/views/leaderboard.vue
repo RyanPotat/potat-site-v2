7e133640e56df2284e438aa91f1e1c75eabdc9ae
ryanpotat <ryan.serenity@gmail.com>
2024-06-27 01:55:05 +0000

    feat: add leaderboard

--- src/views/leaderboard.vue
+++ src/views/leaderboard.vue
@@ -1,11 +1,124 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { brightenColor } from '../assets/utilities';
 
+interface Leaderboard {
+  bestName: string;
+  rank: number;
+  farmSize: number;
+  farmName: string;
+  prestige: number;
+  potatoCount: number;
+  user_pfp: string;
+  user_color: string;
+}
+
+const leaderboarders = ref<Leaderboard[]>([]);
+
+const fetchLeaderboard = async () => {
+  const response = await fetch('https://api.potat.app/leaderboard').then((res) => res.json());
+  leaderboarders.value = response.leaderboard;
+}
+
+onMounted(() => {
+  fetchLeaderboard();
+});
 </script>
 
 <template>
-
+  <div id="container" v-if="leaderboarders.length">
+    <ul class="leaderboard-list">
+      <li v-for="user in leaderboarders" :key="user.bestName" class="leaderboard-item">
+        <div class="profile-picture">
+          <a :href="`https://twitch.tv/${user.bestName.toLowerCase()}`" target="_blank">
+            <img :src="user.user_pfp" alt="profile picture" />
+          </a>
+        </div>
+        <div class="text-content">
+          <div class="basic-info">
+            #{{ user.rank }} 
+            <a :href="`https://twitch.tv/${user.bestName.toLowerCase()}`" target="_blank">
+              <strong :style="{ color: brightenColor(user.user_color) }">{{ user.bestName }}</strong>
+            </a>
+            
+          </div>
+          <div class="details">
+            <div>Prestige: {{ user.prestige }}</div>
+            <div>Potatoes: {{ user.potatoCount.toLocaleString() }}</div>
+            <div>Farm: {{ user.farmName }}</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 
+#container {
+  max-height: 93vh;
+  width: 95%;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  color: #e5e4e4;
+}
+
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  line-height: 1.5;
+  background-color: #3d3d3d;
+}
+
+.leaderboard-list {
+  list-style: none;
+  padding: 0;
+  overflow-y: auto;
+}
+
+.leaderboard-item {
+  padding: 3px;
+  border-radius: 0.5rem;
+  background-color: rgba(31, 31, 31, 0.94);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.leaderboard-item .text-content img {
+  vertical-align: middle;
+}
+
+.profile-container {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background-color: rgba(31, 31, 31, 0.94);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.profile-picture img {
+  border-radius: 50%;
+  margin-right: 5px;
+  width: 80px;
+  height: 80px;
+  vertical-align: middle;
+}
+
+.text-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.leaderboard-list::-webkit-scrollbar {
+  width: 0px;
+  background: transparent;
+}
+
+.leaderboard-list::-webkit-scrollbar-thumb {
+  background: transparent;
+}
 </style>
