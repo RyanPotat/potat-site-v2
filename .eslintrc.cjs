b54b7fdd0d22f9452a4595bd574a4a91c01f31e1
NotForsen <65401513+notforsen@users.noreply.github.com>
2024-10-11 20:20:59 -0500

    Feat(Linter): .editorconfig and fix .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,51 +1,51 @@
 'use strict';
 
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": false,
-    "es2023": true
+  env: {
+    browser: true,
+    commonjs: false,
+    es2023: true
   },
-  "extends": [
-    "airbnb-base/legacy",
-    //"prettier"
+  extends: [
+    'airbnb-base/legacy'
+    // "prettier"
   ],
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.ts"],
-      "parser": "@typescript-eslint/parser",
-      "extends": [
-        "airbnb-base",
-        "plugin:@typescript-eslint/recommended"
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended'
       ],
-      "plugins": ["@typescript-eslint"],
-      "parserOptions": {
-        "ecmaVersion": 2023,
-        "sourceType": "module"
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: 'module'
       },
-      "rules": {
-        "strict": ["error", "global"],
-        "no-eval": "off",
-        "new-cap": "off",
-        "no-promise-executor-return": "off",
-        "no-unused-vars": [
-          "error",
+      rules: {
+        strict: ['error', 'global'],
+        'no-eval': 'off',
+        'new-cap': 'off',
+        'no-promise-executor-return': 'off',
+        'no-unused-vars': [
+          'error',
           {
-            "argsIgnorePattern": "client|msg"
+            argsIgnorePattern: 'client|msg'
           }
         ],
-        "no-await-in-loop": "off",
-        "no-restricted-syntax": "off",
-        "no-continue": "off",
-        "global-require": "off",
-        "no-unused-expressions": "off",
-        "one-var": "off",
-        "no-void": "off",
-        "no-param-reassign": "off",
-        "no-global-assign": "off",
-        "@typescript-eslint/no-unsafe-optional-chaining": "off",
-        "consistent-return": "off"
+        'no-await-in-loop': 'off',
+        'no-restricted-syntax': 'off',
+        'no-continue': 'off',
+        'global-require': 'off',
+        'no-unused-expressions': 'off',
+        'one-var': 'off',
+        'no-void': 'off',
+        'no-param-reassign': 'off',
+        'no-global-assign': 'off',
+        '@typescript-eslint/no-unsafe-optional-chaining': 'off',
+        'consistent-return': 'off'
       }
     }
   ]
-}
+};
